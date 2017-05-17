e26a40462843fa2aeae15bbe7b84ec6aec09e07a
Spencer Smith <spencer.smith@ldschurch.org>
2017-05-17 16:00:25 -0600

    Added settings page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 var app = angular.module('app', [
+  'ngRoute',
   'controllers',
   'customFilters'
 ]);
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/list', {
+    templateUrl: 'partials/list.html',
+    controller: 'List'
+  })
+  .when('/settings', {
+    templateUrl: 'partials/settings.html'
+  })
+  .otherwise({
+    redirectTo: '/list'
+  });
+}]);
