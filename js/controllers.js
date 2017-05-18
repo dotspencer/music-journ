c0cb5ed047f0daabba32b3014952ddcd93a5a6ed
Spencer Smith <spencer.smith@ldschurch.org>
2017-05-18 08:59:34 -0600

    Using controller array notation

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,15 @@
 var control = angular.module('controllers', []);
 
-control.controller('List', function($scope, $http){
+// Controller array notation is used to prevent errors when code is minified
+control.controller('List', ['$scope', '$http', function($scope, $http){
   $http.get('data/songs.json').success(function(data){
     for (var i = 0; i < data.length; i++) {
       data[i].date = new Date(data[i].date);
     }
     $scope.songs = data;
   });
-});
+}]);
 
-control.controller('Player', function($scope, $routeParams){
+control.controller('Player', ['$scope', '$routeParams', function($scope, $routeParams){
   $scope.vid = $routeParams.vid;
-});
+}]);
